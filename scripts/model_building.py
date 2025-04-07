37f2573a71859cd419d65a0420efedfccd10d735
Michał Sobieraj <148006692+Michal-Sobieraj@users.noreply.github.com>
2025-04-08 00:02:43 +0200

    added best_estimator_

--- scripts/model_building.py
+++ scripts/model_building.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import copy
 import numpy as np
 import pandas as pd
 import scipy
@@ -65,8 +66,9 @@ def process(benchmark_collection, coll_folder, bench_name, prediction_collection
     
     # save true prop
     res_train['Y_TRUE'] = [i[1] for i in data_train]
+    res_train_default['Y_TRUE'] = [i[1] for i in data_train]
     res_test['Y_TRUE'] = [i[1] for i in data_test]
-    
+    res_test_default['Y_TRUE'] = [i[1] for i in data_test]
     # calc 2D descriptors
     for descr_name in descr_names:
         descr_func = descr_dict.get(descr_name)
@@ -78,6 +80,7 @@ def process(benchmark_collection, coll_folder, bench_name, prediction_collection
             continue
         
         y_train = [i[1] for i in data_train]
+        y_test = [i[1] for i in data_test]
 
         # scale training data descriptors
         scaler = MinMaxScaler()
@@ -89,12 +92,7 @@ def process(benchmark_collection, coll_folder, bench_name, prediction_collection
         # train machine learning model
         for method_name in ml_dict:
 
-            print(bench_name, descr_name, method_name, flush=True)  # show progress
-            sys.stdout.write(' '.join((bench_name, descr_name, method_name)) + '\n')
-            sys.stdout.flush()
-
-            model_empty = deepcopy(ml_dict.get(method_name))  # check whether it is needed
-            
+            # print(bench_name, descr_name, method_name, flush=True)  
             model = ml_dict.get(method_name)
 
             # concat cross-validation prediction from the training set for consensus and stacking building
@@ -103,26 +101,26 @@ def process(benchmark_collection, coll_folder, bench_name, prediction_collection
 
             # build the final 2D model
             model.fit(x_train_scaled, y_train)
-            
+
             # make test set predictions
             y_pred = model.predict(x_test_scaled)
-            res_test[f'{descr_name}|{method_name}'] = y_pred
-            # to csv default test
+            res_test_default[f'{descr_name}|{method_name}'] = y_pred
 
             # make test set predictions with hyperparameters optimized
-            model = (RandomizedSearchCV(model, hyper_parameters.get(method_name), n_iter=30, cv=cv, error_score='raise', n_jobs=1))
-            model.fit(x_train_scaled, y_train)
-            y_pred = model.predict(x_test_scaled)
+            random_search = (RandomizedSearchCV(model, hyper_parameters.get(method_name), n_iter=30, cv=cv, error_score= "raise", n_jobs=1))
+            random_search.fit(x_train_scaled, y_train)
+            best_model = random_search.best_estimator_
+            y_pred = best_model.predict(x_test_scaled)
             res_test[f'{descr_name}|{method_name}_RSCV'] = y_pred
-            res_test.to_csv(os.path.join(res_folder, f'{bench_name}_test.csv'), index=False)
 
-            y_pred = cross_val_predict(model_empty(**model.best_params_), x_train_scaled, y_train, cv=cv, n_jobs=1)   # check and remove one of lines
-            y_pred = cross_val_predict(partial(model_empty, **model.best_params_), x_train_scaled, y_train, cv=cv, n_jobs=1)
-            y_pred = cross_val_predict(model, x_train_scaled, y_train, cv=cv, n_jobs=1)
-            # to csv
+            # make test set predictions with stacking with hyperparameters optimized
+            y_pred = cross_val_predict(best_model, x_test_scaled, y_test, cv=cv, n_jobs=1)
+            res_test[f'{descr_name}|{method_name}_stacking_RSCV'] = y_pred
 
+    # to csv
     res_train_default.to_csv(os.path.join(res_folder, f'{bench_name}_default_traincv.csv'), index=False)
-
+    res_test_default.to_csv(os.path.join(res_folder, f'{bench_name}_default_test.csv'), index=False)
+    res_test.to_csv(os.path.join(res_folder, f'{bench_name}_test.csv'), index=False)
 
 def prepare(benchmark_collection):
     output = []
@@ -133,29 +131,29 @@ def prepare(benchmark_collection):
 
 
 descr_dict = {
-    'scaffoldkeys': MoleculeTransformer(featurizer='scaffoldkeys', dtype=float),
+    # 'scaffoldkeys': MoleculeTransformer(featurizer='scaffoldkeys', dtype=float),
     'secfp': MoleculeTransformer(featurizer='secfp', dtype=float),
-    'atompair-count': MoleculeTransformer(featurizer='atompair-count', dtype=float),
-    'avalon': MoleculeTransformer(featurizer='avalon', dtype=float),
-    'ecfp-count': MoleculeTransformer(featurizer='ecfp-count', dtype=float),
-    'ecfp': MoleculeTransformer(featurizer='ecfp', dtype=float),
-    'erg': MoleculeTransformer(featurizer='erg', dtype=float),
-    'estate': MoleculeTransformer(featurizer='estate', dtype=float),
-    'fcfp-count': MoleculeTransformer(featurizer='fcfp-count', dtype=float),
-    'fcfp': MoleculeTransformer(featurizer='fcfp', dtype=float),
-    'maccs': MoleculeTransformer(featurizer='maccs', dtype=float),
-    'pattern': MoleculeTransformer(featurizer='pattern', dtype=float),
-    'rdkit': MoleculeTransformer(featurizer='rdkit', dtype=float),
-    'topological-count': MoleculeTransformer(featurizer='topological-count', dtype=float),
-    'topological': MoleculeTransformer(featurizer='topological', dtype=float),
-    'layered': MoleculeTransformer(featurizer='layered', dtype=float),
-    'erg': FPVecTransformer(kind='erg', dtype=float),
+    # 'atompair-count': MoleculeTransformer(featurizer='atompair-count', dtype=float),
+    # 'avalon': MoleculeTransformer(featurizer='avalon', dtype=float),
+    # 'ecfp-count': MoleculeTransformer(featurizer='ecfp-count', dtype=float),
+    # 'ecfp': MoleculeTransformer(featurizer='ecfp', dtype=float),
+    # 'erg': MoleculeTransformer(featurizer='erg', dtype=float),
+    # 'estate': MoleculeTransformer(featurizer='estate', dtype=float),
+    # 'fcfp-count': MoleculeTransformer(featurizer='fcfp-count', dtype=float),
+    # 'fcfp': MoleculeTransformer(featurizer='fcfp', dtype=float),
+    # 'maccs': MoleculeTransformer(featurizer='maccs', dtype=float),
+    # 'pattern': MoleculeTransformer(featurizer='pattern', dtype=float),
+    # 'rdkit': MoleculeTransformer(featurizer='rdkit', dtype=float),
+    # 'topological-count': MoleculeTransformer(featurizer='topological-count', dtype=float),
+    # 'topological': MoleculeTransformer(featurizer='topological', dtype=float),
+    # 'layered': MoleculeTransformer(featurizer='layered', dtype=float),
+    # 'erg': FPVecTransformer(kind='erg', dtype=float),
 
     #long
-    'desc2D': MoleculeTransformer(featurizer='desc2D', dtype=float),
-    'pharm2D-cats': MoleculeTransformer(featurizer=Pharmacophore2D(factory='cats'), dtype=float),
-#    'pharm2D-gobbi': MoleculeTransformer(featurizer=Pharmacophore2D(factory='gobbi'), dtype=float),
-    'pharm2D-pmapper': MoleculeTransformer(featurizer=Pharmacophore2D(factory='pmapper'), dtype=float),
+    # 'desc2D': MoleculeTransformer(featurizer='desc2D', dtype=float),
+    # 'pharm2D-cats': MoleculeTransformer(featurizer=Pharmacophore2D(factory='cats'), dtype=float),
+    # 'pharm2D-gobbi': MoleculeTransformer(featurizer=Pharmacophore2D(factory='gobbi'), dtype=float),
+    # 'pharm2D-pmapper': MoleculeTransformer(featurizer=Pharmacophore2D(factory='pmapper'), dtype=float),
     
     # Need additional dependencies
     # 'map4': MoleculeTransformer(featurizer='map4', dtype=float),
@@ -189,7 +187,6 @@ hyper_parameters = {
     'PLSRegression': {
         'n_components': randint(1, 11),
         'scale': [True, False],
-#        'max_iter': [500, 1000, 2000],
         'tol': loguniform(1e-6, 1e-3)
     },
     'RidgeRegression': {
@@ -198,40 +195,31 @@ hyper_parameters = {
         'fit_intercept': [True, False],
         'max_iter': [None, 1000, 5000],
         'tol': [1e-4, 1e-3, 1e-2]
-    },
+   },
     'LinearRegression': {
         'fit_intercept': [True, False],
-        'positive': [True, False]
     },
     'RandomForestRegressor': {
         'n_estimators': randint(50, 500),
         'max_depth': [None, 10, 25, 50],
-        'max_features': ['sqrt', 'log2', 0.3, 0.5],
+        'max_features': ['sqrt', 0.3, 0.5],
         'min_samples_split': [2, 5, 10],
         'min_samples_leaf': [1, 2, 4],
         'bootstrap': [True]
     },
     'MLPRegressor': {
-        'hidden_layer_sizes': [(50,), (100,), (50,50), (100,50)],
-        'activation': ['relu', 'tanh', 'logistic'],
-#        'solver': ['adam', 'sgd', 'lbfgs'],
-        'solver': ['sgd'],
-        'alpha': loguniform(1e-5, 1e-1),
-        'learning_rate': ['constant', 'invscaling', 'adaptive'],
-        'max_iter': [2000, 5000]
+        'hidden_layer_sizes': [(50,), (100,)],
+        'solver': ['adam'],
+        'max_iter': [2000, 5000],
     },
-    'SVR': {
-        'C': loguniform(1e-3, 1e3),
-        'epsilon': uniform(0.01, 0.3),
-        'kernel': ['rbf', 'linear', 'poly'],
-        'gamma': loguniform(1e-5, 1),
-#        'degree': [2, 3]
-    },
+'SVR': {
+    'C': [0.1, 1, 10],  
+    'kernel': ['rbf'],  
+    'gamma': [0.01, 0.1, 1]
+},
     'KNeighborsRegressor': {
         'n_neighbors': randint(1, 50),
         'weights': ['uniform', 'distance'],
-#        'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
-#        'p': [1, 2],
         'metric': ['minkowski', 'euclidean', 'manhattan']
     }
 }
